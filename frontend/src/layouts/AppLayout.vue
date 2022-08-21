f0d28d1a8906285b373942ac85c380242eb0b4a3
Vitaly Bochkarev <vitalybochkarev@Book.local>
2022-08-21 21:38:23 +0700

    import AppHeader

--- frontend/src/layouts/AppLayout.vue
+++ frontend/src/layouts/AppLayout.vue
@@ -8,11 +8,3 @@
 <script setup>
 import AppHeader from "./AppHeader.vue";
 </script>
-
-<style lang="scss" scoped>
-.app_layout {
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-}
-</style>
