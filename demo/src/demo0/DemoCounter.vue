245a8db0e1e3b63bf7ffea0d0f83fffc083afe2c
Vitaly Bochkarev <vitalybochkarev@Book.local>
2022-08-21 13:29:18 +0700

    demo0

---
+++ demo/src/demo0/DemoCounter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button> - </button>
+
+    <span> Current value: 0 </span>
+
+    <button> + </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DemoCounter"
+}
+</script>
+
+<style scoped>
+
+</style>
