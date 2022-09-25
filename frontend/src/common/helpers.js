163a33991993b596c289b367ad38c63ab94d59a9
Vitaly Bochkarev <vitalybochkarev@Book.local>
2022-09-26 00:09:12 +0700

    refactor HomeView

--- frontend/src/common/helpers.js
+++ frontend/src/common/helpers.js
@@ -30,3 +30,9 @@ export const normalizeTask = task => {
         timeStatus: getTimeStatus(task.dueDate)
     };
 };
+
+
+export const getImage = image => {
+    // https://vitejs.dev/guide/assets.html#new-url-url-import-meta-url
+    return new URL(`../assets/img/${image}`, import.meta.url).href
+}
