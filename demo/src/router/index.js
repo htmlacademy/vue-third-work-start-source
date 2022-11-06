ecc635501cd64b74d8c7a87efb8613eb816b7cc6
Evgeniy Kozlov <kozlov.work.52@gmail.com>
2022-11-06 14:14:50 +0600

    Sync with template

--- demo/src/router/index.js
+++ demo/src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
+import routes from '../routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +8,8 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: () => import("../components/DemoMenu.vue"),
-    }
+    },
+    ...routes,
   ],
 });
 
