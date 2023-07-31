0de1bd023cb0f5ffeadac851a72a8a978d954eb1
Ksenia <kseniarlog@gmail.com>
2023-07-31 17:25:55 +0300

    feat: компонент шапки #1.18

---
+++ frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="app_layout">
+    <app-layout-header />
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import { AppLayoutHeader } from "@/layouts";
+</script>
---
+++ frontend/src/layouts/index.js
@@ -0,0 +1,2 @@
+export { default as AppLayoutHeader } from "./AppLayoutHeader.vue";
+export { default as AppLayout } from "./AppLayout.vue";
