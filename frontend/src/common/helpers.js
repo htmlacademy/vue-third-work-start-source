4028e360e39de20338f02b2d658bcfcb71216e65
Ksenia <kseniarlog@gmail.com>
2023-08-02 23:39:15 +0300

    feat: константы и хэлперы #2.5

--- frontend/src/common/helpers.js
+++ frontend/src/common/helpers.js
@@ -1,3 +1,4 @@
+import { toRaw } from "vue";
 import { DAY_IN_MILLISEC, TAG_SEPARATOR } from "./constants";
 import timeStatuses from "./enums/timeStatuses";
 import taskStatuses from "./enums/taskStatuses";
@@ -23,4 +24,31 @@ export const normalizeTask = (task) => {
     status: task.statusId ? taskStatuses[task.statusId] : "",
     timeStatus: getTimeStatus(task.dueDate),
   };
+};
+
+export const getImage = (image) => {
+  return new URL(`../assets/img/${image}`, import.meta.url).href;
+};
+
+export const getTargetColumnTasks = (toColumnId, tasks) => {
+  return tasks
+    .filter((task) => task.columnId === toColumnId)
+    .map((task) => toRaw(task));
+};
+
+export const addActive = (active, toTask, tasks) => {
+  const activeIndex = tasks.findIndex((task) => task.id === active.id);
+  if (~activeIndex) {
+    tasks.splice(activeIndex, 1);
+  }
+
+  tasks.sort((a, b) => a.sortOrder - b.sortOrder);
+
+  if (toTask) {
+    const toTaskIndex = tasks.findIndex((task) => task.id === toTask.id);
+    tasks.splice(toTaskIndex, 0, active);
+  } else {
+    tasks.push(active);
+  }
+  return tasks;
 };
