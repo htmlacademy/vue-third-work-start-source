b2e484a0b022dd9a40d789bbc12d970d4a1bfd22
Alexander <xandr-g@yandex.ru>
2024-12-05 10:05:58 +0300

    5.5. Создаём сервисы в проекте Vue-Work

---
+++ demo/src/services/providers/index.js
@@ -0,0 +1,18 @@
+
+import FetchProvider from './FetchProvider'
+import { useAuthStore } from '@/stores'
+
+const httpProvider = new FetchProvider()
+httpProvider.addInterceptor(
+    {
+        onError: async (status) => {
+            // Если с сервиса приходит ошибка 401 Unauthorized, выполняем выход из системы
+            if (status === 401) {
+                const authStore = useAuthStore()
+                if (authStore.isAuthenticated) await authStore.logout()
+            }
+        },
+    }
+)
+
+export default httpProvider
